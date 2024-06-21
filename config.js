c13cc52e608343a6780e3990fa89146cf6068cf5
imalk0303 <deployfree99@gmail.com>
2024-06-21 19:02:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -30,11 +30,11 @@ global.prefa = process.env.PREFIX ? process.env.PREFIX.split(",") : ["."];
 
 
 // Owner information
-global.Owner = process.env.OWNER ? process.env.OWNER.split(",") : ["916297175943", "919362639362"];
-global.OwnerNumber = process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.split(",") : ["916297175943", "919362639362"];
-global.ownertag = process.env.OWNER_TAG ? process.env.OWNER_TAG.split(",") : ["916297175943"];
-global.OwnerName = process.env.OWNER_NAME || "Kai";
-global.BotName = process.env.BOT_NAME || "A17";
+global.Owner = process.env.OWNER ? process.env.OWNER.split(",") : ["94784198085"];
+global.OwnerNumber = process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.split(",") : ["94784198085"];
+global.ownertag = process.env.OWNER_TAG ? process.env.OWNER_TAG.split(",") : ["94784198085"];
+global.OwnerName = process.env.OWNER_NAME || "HANSAMAL";
+global.BotName = process.env.BOT_NAME || "CHUTY";
 global.packname = process.env.PACK_NAME || "A17 Bot";
 global.author = "By: Kai";
 global.BotSourceCode = "https://github.com/Kai0071/A17";
